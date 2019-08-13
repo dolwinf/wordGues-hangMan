1d3e6855fd7fc13c6b10cc48423befa069f046c7
Dolwin Fernandes <dolwinf@gmail.com>
2019-08-13 12:26:47 +1000

    Added core hangman game functionality

--- assets/javascript/gameLogic.js
+++ assets/javascript/gameLogic.js
@@ -1,24 +1,42 @@
 var words = [
-	"sydney",
-	"moscow",
-	"newyork",
-	"florida",
-	"brisbane",
-	"dubai",
-	"mumbai",
-	"rome",
-	"jerusalem",
-	"toronto"
+  "sydney",
+  "moscow",
+  "newyork",
+  "florida",
+  "brisbane",
+  "dubai",
+  "mumbai",
+  "rome",
+  "jerusalem",
+  "toronto"
 ];
 
 var randomWord = words[Math.floor(Math.random() * words.length)];
-console.log(randomWord);
+var randomWordSplit = randomWord.split("");
+console.log(randomWordSplit);
 
 var dashedWords = [];
-for (var i = 0; i < randomWord.length; i++) {
-	dashedWords[i] = "_";
+for (var i = 0; i < randomWordSplit.length; i++) {
+  dashedWords[i] = "_";
 }
 
-dashedWords.join(" ");
-
 document.getElementById("test").innerHTML = dashedWords;
+
+document.addEventListener("keyup", function(event) {
+  var keyPressed = event.key;
+  if (randomWordSplit.includes(keyPressed)) {
+    var index = randomWordSplit.indexOf(keyPressed);
+    dashedWords[index] = keyPressed;
+    document.getElementById("test").innerHTML = dashedWords;
+    randomWordSplit[index] = 0;
+    const check = randomWordSplit.every(function(item) {
+      if (item == 0) {
+        return true;
+      }
+    });
+
+    if (check) {
+      document.getElementById("test2").innerHTML = "Winner!";
+    }
+  }
+});
