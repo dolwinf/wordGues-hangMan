e9a20939978354e7ae711bace66b770e95f6ebc9
shell <dolwinf@gmail.com>
2019-08-14 21:52:53 +1000

    Added images changing based on loss or win

--- assets/javascript/gameLogic.js
+++ assets/javascript/gameLogic.js
@@ -1,14 +1,14 @@
 var words = [
-  "sydney",
-  "moscow",
-  "newyork",
-  "florida",
-  "brisbane",
-  "dubai",
-  "mumbai",
-  "rome",
-  "jerusalem",
-  "toronto"
+	"sydney",
+	"moscow",
+	"newyork",
+	"florida",
+	"brisbane",
+	"dubai",
+	"mumbai",
+	"rome",
+	"jerusalem",
+	"toronto"
 ];
 
 var randomWord = words[Math.floor(Math.random() * words.length)];
@@ -17,7 +17,7 @@ console.log(randomWordSplit);
 
 var dashedWords = [];
 for (var i = 0; i < randomWordSplit.length; i++) {
-  dashedWords[i] = "_ ";
+	dashedWords[i] = "_ ";
 }
 
 document.getElementById("test").innerHTML = dashedWords;
@@ -26,28 +26,32 @@ guessesRemaining = 15;
 document.getElementById("guesses-label").textContent = guessesRemaining;
 
 document.addEventListener("keyup", function(event) {
-  var keyPressed = event.key;
-  if (randomWordSplit.includes(keyPressed)) {
-    var index = randomWordSplit.indexOf(keyPressed);
-    dashedWords[index] = keyPressed;
-    document.getElementById("test").innerHTML = dashedWords;
-    randomWordSplit[index] = 0;
-    const check = randomWordSplit.every(function(item) {
-      if (item == 0) {
-        return true;
-      }
-    });
+	var keyPressed = event.key;
+	if (randomWordSplit.includes(keyPressed)) {
+		var index = randomWordSplit.indexOf(keyPressed);
+		dashedWords[index] = keyPressed;
+		document.getElementById("test").innerHTML = dashedWords;
+		randomWordSplit[index] = 0;
+		const check = randomWordSplit.every(function(item) {
+			if (item == 0) {
+				return true;
+			}
+		});
 
-    if (check) {
-      document.getElementById("test2").textContent =
-        "Winner! You saved Jhonny's SOUL!!!!!!!";
-    }
-  } else {
-    guessesRemaining--;
-    document.getElementById("guesses-label").textContent = guessesRemaining;
-    if (guessesRemaining < 1) {
-      document.getElementById("guesses-label").textContent =
-        "You've LOST! Restart the game before the devil takes your soul.";
-    }
-  }
+		if (check) {
+			document.getElementById("test2").textContent =
+				"Winner! You saved Jhonny's SOUL!!!!!!!";
+			document.getElementById("image").src = "assets/images/devil-defeated.jpg";
+			document.getElementById("image").style = "width: 300px; height: 300px";
+		}
+	} else {
+		guessesRemaining--;
+		document.getElementById("guesses-label").textContent = guessesRemaining;
+		if (guessesRemaining < 1) {
+			document.getElementById("guesses-label").textContent =
+				"You've LOST! Restart the game before the devil takes your soul.";
+			document.getElementById("image").src = "assets/images/devil-cranky.gif";
+			document.getElementById("image").style = "width: 300px; height: 300px";
+		}
+	}
 });
